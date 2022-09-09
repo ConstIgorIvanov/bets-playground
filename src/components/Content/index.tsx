fd868e8ce56e917fa0f6e6ca663dd969ecae7696
Ivanov I.A <constigorivanov@gmail.com>
2022-09-10 03:06:40 +0500

    mini refactor styles

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Wrapper from '../Wrapper';
-import UpcomingTeamCard from './components/UpcomingMatches/components/UpcomingTeamCard/UpcomingTeamCard';
 
 import styles from './Content.module.scss';
 
