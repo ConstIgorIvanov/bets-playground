7fb3f0d183bee0d67ce72a8633fb915347f1b4ae
Ivanov I.A <constigorivanov@gmail.com>
2022-09-11 07:29:15 +0500

    content layout start

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Wrapper from '../Wrapper';
+import UpcomingMatches from './components/UpcomingMatches/UpcomingMatches';
 
 import styles from './Content.module.scss';
 
 const Content = () => {
-  return <div className={styles.content}></div>;
+  return (
+    <div className={styles.content}>
+      <div className={styles.title}>Текущие матчи</div>
+      <UpcomingMatches />
+    </div>
+  );
 };
 
 export default Wrapper(Content);
