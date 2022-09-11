74e370b77c9e29b014ee0c363e010b49dae374f0
Ivanov I.A <constigorivanov@gmail.com>
2022-09-11 08:19:15 +0500

    Content wrapper, layout styles

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Wrapper from '../Wrapper';
+import LiveMatches from './components/LiveMatches/LiveMatches';
 import UpcomingMatches from './components/UpcomingMatches/UpcomingMatches';
 
 import styles from './Content.module.scss';
@@ -7,8 +8,14 @@ import styles from './Content.module.scss';
 const Content = () => {
   return (
     <div className={styles.content}>
-      <div className={styles.title}>Текущие матчи</div>
-      <UpcomingMatches />
+      <div className={styles.content__wrapper}>
+        <div className={styles.title}>Текущие матчи</div>
+        <LiveMatches />
+      </div>
+      <div className={styles.content__wrapper}>
+        <div className={styles.title}>Ближайшие матчи</div>
+        <UpcomingMatches />
+      </div>
     </div>
   );
 };
