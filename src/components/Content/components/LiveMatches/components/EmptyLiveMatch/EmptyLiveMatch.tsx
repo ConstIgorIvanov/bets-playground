3f3a772a992e4e041dbe955bb8be487af57ee59b
Ivanov I.A <constigorivanov@gmail.com>
2022-09-11 12:01:50 +0500

    emptylivematch timer animation

---
+++ src/components/Content/components/LiveMatches/components/EmptyLiveMatch/EmptyLiveMatch.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './EmptyLiveMatch.module.scss';
+
+const EmptyLiveMatch = () => {
+  return <div className={styles.timer_animated}></div>;
+};
+
+export default EmptyLiveMatch;
