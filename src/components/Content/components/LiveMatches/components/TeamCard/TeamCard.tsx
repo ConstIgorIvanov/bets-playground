e9a1b03108f7099c3b3c48353a84351e094b18da
Ivanov I.A <constigorivanov@gmail.com>
2022-09-11 11:21:24 +0500

    TeamCard Image Mask fix

--- src/components/Content/components/LiveMatches/components/TeamCard/TeamCard.tsx
+++ src/components/Content/components/LiveMatches/components/TeamCard/TeamCard.tsx
@@ -6,18 +6,18 @@ import styles from './TeamCard.module.scss';
 const TeamCard = ({ imageLink, name, odd }: TeamCardI) => {
   return (
     <div className={styles.card}>
-      <div className={styles.card__imgContainer}>
-        <img className={styles.card__img} src={imageLink} alt="Team Logo" />
+      <div className={styles.card__img__wrapper}>
+        <div className={styles.card__imgContainer}>
+          <img className={styles.card__img} src={imageLink} alt="Team Logo" />
+        </div>
+        <div className={styles.card__img__mask}>
+          <div className={styles.card__img__mask__plus}>+</div>
+          <div className={styles.card__img__mask__text}>Сделать ставку</div>
+        </div>
       </div>
       <div className={styles.card__name}>{name}</div>
       <span className={styles.card__odd}>{odd}</span>
-      <div className={styles.card__mask}>
-        {' '}
-        <div className={styles.card__mask__text}>
-          <div className={styles.card__mask__plus}>+</div>
-          <div className={styles.card__mask__text}>Сделать ставку</div>
-        </div>
-      </div>
+      <div className={styles.card__mask}> </div>
     </div>
   );
 };
