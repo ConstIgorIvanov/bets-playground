14d4365c97dc6c3f08fae3938bfc6bf004a5c6df
Ivanov I.A <constigorivanov@gmail.com>
2022-09-11 11:40:59 +0500

    Live Matches Layout end

--- src/components/Content/components/LiveMatches/components/LiveMatchesItem/LiveMatchesItem.tsx
+++ src/components/Content/components/LiveMatches/components/LiveMatchesItem/LiveMatchesItem.tsx
@@ -3,12 +3,21 @@ import React from 'react';
 import { LiveMatchesItemI } from './LiveMatchesItem.props';
 
 import styles from './LiveMatchesItem.module.scss';
+import TeamCard from '../TeamCard/TeamCard';
 
 const LiveMatchesItem = (props: LiveMatchesItemI) => {
   return (
     <div className={styles.liveMatchesItem}>
-      <div>index</div>
-      <div>index</div>
+      <TeamCard imageLink="https://csgopositive.co/img/7157.svg" name="rogueroguerogue" odd="1.2" />
+      <div className={styles.matchInfo}>
+        <span className={styles.game}>
+          <img src="https://csgopositive.co/template_v2/img/valorant_bg.png" />
+        </span>
+        <span className={styles.tournament}>LEC</span>
+        <div className={styles.format}>BO5</div>
+        <div className={styles.time}>09:04:12</div>
+      </div>
+      <TeamCard imageLink="https://csgopositive.co/img/G2.png" name="g2" odd="2" />
     </div>
   );
 };
